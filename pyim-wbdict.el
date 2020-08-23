cd8a1c431714b06fd878d766306cc90e7d60bbf2
Yuanchen Xie <firstarfox@foxmail.com>
2020-08-23 23:12:21 +0800

    prefix , to .

--- pyim-wbdict.el
+++ pyim-wbdict.el
@@ -33,7 +33,8 @@
 
 ;; ** 简介
 ;; pyim-wbdict 是 pyim 的一个五笔词库包。
-;; 1. pyim-wbdict-v98.pyim 源于 emacs-eim 的五笔词库。
+;; 1. pyim-wbdict-v86.pyim 源于 emacs-eim 的五笔词库。
+;; 2. pyim-wbdict-v98.pyim 源于 98wubi-tables 的五笔词库。
 
 ;; ** 安装和使用
 ;; 1. 配置melpa源，参考：http://melpa.org/#/getting-started
@@ -41,8 +42,8 @@
 ;; 3. 在emacs配置文件中（比如: ~/.emacs）添加如下代码：
 ;;    #+BEGIN_EXAMPLE
 ;;    (require 'pyim-wbdict)
-;;    (pyim-wbdict-v86-enable) ;; 86版用户
-;;    (pyim-wbdict-v98-enable) ;; 98版用户
+;;    ;; (pyim-wbdict-v86-enable) ;; 86版用户
+;;    ;; (pyim-wbdict-v98-enable) ;; 98版用户
 ;;    #+END_EXAMPLE
 
 ;;; Code:
