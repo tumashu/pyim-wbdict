2766bf0dd8514226cd8aac1ab9402af603b96d06
Feng Shu <tumashu@163.com>
2023-01-13 15:44:41 +0800

    v0.3.0

--- pyim-wbdict.el
+++ pyim-wbdict.el
@@ -5,7 +5,7 @@
 
 ;; Author: Feng Shu <tumashu@163.com>
 ;; URL: https://github.com/tumashu/pyim-wbdict
-;; Version: 0.2.0
+;; Version: 0.3.0
 ;; Package-Requires: ((pyim "3.7"))
 ;; Keywords: convenience, Chinese, pinyin, input-method, complete
 
